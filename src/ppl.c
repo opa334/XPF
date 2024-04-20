f2aaf5143f86e2338e0c8e25f2f3b1fd67a1a7ed
opa334 <opa334@users.noreply.github.com>
2024-04-20 18:04:26 +0200

    Switch to pmap_enter_pv for finding pv_head_table, fixes 15.0b1 support

--- src/ppl.c
+++ src/ppl.c
@@ -206,54 +206,36 @@ static uint64_t xpf_find_pmap_pin_kernel_pages_reference(uint32_t idx)
 	return ref;
 }
 
-static uint64_t xpf_find_tte_get_ptd(void)
+static uint64_t xpf_find_pmap_enter_pv(void)
 {
-	PFStringMetric *stringMetric = pfmetric_string_init("tte_get_ptd");
-	__block uint64_t tte_get_ptd_stringAddr = 0;
+	PFStringMetric *stringMetric = pfmetric_string_init("pmap_enter_pv");
+	__block uint64_t pmap_enter_pv_stringAddr = 0;
 	pfmetric_run(gXPF.kernelStringSection, stringMetric, ^(uint64_t vmaddr, bool *stop){
-		tte_get_ptd_stringAddr = vmaddr;
+		pmap_enter_pv_stringAddr = vmaddr;
 		*stop = true;
 	});
 	pfmetric_free(stringMetric);
 
-	if (!tte_get_ptd_stringAddr) {
-		// iOS 15.0.x don't have the "tte_get_ptd" string
-		stringMetric = pfmetric_string_init("%s: Passed in pmap doesn't own the page table to be deleted ptd=%p ptd->pmap=%p pmap=%p @%s:%d");
-		pfmetric_run(gXPF.kernelStringSection, stringMetric, ^(uint64_t vmaddr, bool *stop){
-			tte_get_ptd_stringAddr = vmaddr;
-			*stop = true;
-		});
-		pfmetric_free(stringMetric);
-	}
-
-	PFXrefMetric *xrefMetric = pfmetric_xref_init(tte_get_ptd_stringAddr, XREF_TYPE_MASK_REFERENCE);
-	__block uint64_t tte_get_ptd = 0;
-	pfmetric_run(gXPF.kernelTextSection, xrefMetric, ^(uint64_t vmaddr, bool *stop) {
-		tte_get_ptd = pfsec_find_function_start(gXPF.kernelTextSection, vmaddr);
+	PFXrefMetric *xrefMetric = pfmetric_xref_init(pmap_enter_pv_stringAddr, XREF_TYPE_MASK_REFERENCE);
+	__block uint64_t pmap_enter_pv = 0;
+	pfmetric_run(gXPF.kernelPPLTextSection, xrefMetric, ^(uint64_t vmaddr, bool *stop) {
+		pmap_enter_pv = pfsec_find_function_start(gXPF.kernelPPLTextSection, vmaddr);
 		*stop = true;
 	});
 	pfmetric_free(xrefMetric);
 
-	return tte_get_ptd;
+	return pmap_enter_pv;
 }
 
 static uint64_t xpf_find_pv_head_table(void)
 {
-	uint64_t tte_get_ptd = xpf_item_resolve("kernelSymbol.tte_get_ptd");
+	uint64_t pmap_enter_pv = xpf_item_resolve("kernelSymbol.pmap_enter_pv");
 
 	uint32_t ldrAnyInst = 0, ldrAnyMask = 0;
 	arm64_gen_ldr_imm(0, LDR_STR_TYPE_UNSIGNED, ARM64_REG_ANY, ARM64_REG_ANY, OPT_UINT64_NONE, &ldrAnyInst, &ldrAnyMask);
 
-	uint64_t imm = 0;
-	uint64_t ref1 = tte_get_ptd;
-	while (imm == 0) {
-		ref1 = pfsec_find_next_inst(gXPF.kernelTextSection, ref1, 0, ldrAnyInst, ldrAnyMask);
-		arm64_dec_ldr_imm(pfsec_read32(gXPF.kernelTextSection, ref1), NULL, NULL, &imm, NULL, NULL);
-		ref1 += 4;
-	}
-	uint64_t ref2 = pfsec_find_next_inst(gXPF.kernelTextSection, ref1, 0, ldrAnyInst, ldrAnyMask);
-
-	return pfsec_arm64_resolve_adrp_ldr_str_add_reference_auto(gXPF.kernelTextSection, ref2);
+	uint64_t ref = pfsec_find_next_inst(gXPF.kernelPPLTextSection, pmap_enter_pv, 0, ldrAnyInst, ldrAnyMask);
+	return pfsec_arm64_resolve_adrp_ldr_str_add_reference_auto(gXPF.kernelPPLTextSection, ref);
 }
 
 static uint64_t xpf_find_pmap_enter_options_addr(void)
@@ -307,7 +289,7 @@ void xpf_ppl_init(void)
 		xpf_item_register("kernelSymbol.pmap_remove_options_ppl", xpf_find_ppl_dispatch_func, (void *)(uint32_t)23);
 		xpf_item_register("kernelSymbol.pmap_lookup_in_loaded_trust_caches_internal", xpf_find_ppl_routine, (void *)(uint32_t)41);
 		xpf_item_register("kernelSymbol.pmap_pin_kernel_pages", xpf_find_pmap_pin_kernel_pages, NULL);
-		xpf_item_register("kernelSymbol.tte_get_ptd", xpf_find_tte_get_ptd, NULL);
+		xpf_item_register("kernelSymbol.pmap_enter_pv", xpf_find_pmap_enter_pv, NULL);
 
 		xpf_item_register("kernelSymbol.pmap_enter_options_addr", xpf_find_pmap_enter_options_addr, NULL);
 		xpf_item_register("kernelSymbol.pmap_remove_options", xpf_find_pmap_remove_options, NULL);
