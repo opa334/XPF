55d42d8136183fcd9d356098f8b3db22cf8ee9a8
opa334 <opa334@users.noreply.github.com>
2026-02-05 12:15:26 +0100

    XPF_ASSERT: Always keep first assert that failed

--- src/xpf.h
+++ src/xpf.h
@@ -22,7 +22,7 @@ typedef struct s_XPFSet {
 	const char *metrics[];
 } XPFSet;
 
-#define XPF_ASSERT(assert) if (!(assert)) { xpf_set_error("[%s:%d] Failed assert in %s: %s", __FILE__, __LINE__, __FUNCTION__, #assert); return 0; }
+#define XPF_ASSERT(assert) if (!(assert)) { if (!xpf_get_error()) { xpf_set_error("[%s:%d] Failed assert in %s: %s", __FILE__, __LINE__, __FUNCTION__, #assert); } return 0; }
 
 int xpf_start_with_kernel_path(const char *kernelPath);
 void xpf_item_register(const char *name, void *finder, void *ctx);
